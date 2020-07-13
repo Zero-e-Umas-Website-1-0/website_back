37b973e6ca026cce597ed70e85228df884c3e00d
paulaleite <48655864+paulaleite@users.noreply.github.com>
2020-07-13 10:57:33 -0300

    Changes to code like Gois

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
