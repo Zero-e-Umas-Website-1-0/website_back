0fcdd07dd3bda284b1329e6897ac79ddd59f0525
liakassardjian <liakassardjian@gmail.com>
2020-07-14 10:16:56 -0300

    Adicionado Swagger para visualizar a API completa

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Central de atendimento 0&Umas')
+    .setDescription('Aqui você pode ver todas as mensagens enviadas através de nosso site')
+    .setVersion('0.0.1')
+    .addTag('0eumas')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
